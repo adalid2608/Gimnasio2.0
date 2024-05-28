b8aa32bae69d5b2f6d618af7ce3548e9cd20c5bf
adalid2608 <adalid.islas26@gmail.com>
2024-05-28 10:52:45 -0600

    Creación de la ruta para el regsitro de usuarios

--- FrontEnd/Gimnasio-FrontEnd/src/router/index.js
+++ FrontEnd/Gimnasio-FrontEnd/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/components/LoginView.vue'
+import RegisterView from '@/components/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'login',
       component: LoginView
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: RegisterView
     }
   ]
 })

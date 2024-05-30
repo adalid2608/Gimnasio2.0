22061f10733c9cc0d3b3ff662038dfa4fefd2f36
adalid2608 <adalid.islas26@gmail.com>
2024-05-30 08:56:33 -0600

    Actualizacion de campos en el formulario de registro

--- FrontEnd/Gimnasio-FrontEnd/src/components/RegisterView.vue
+++ FrontEnd/Gimnasio-FrontEnd/src/components/RegisterView.vue
@@ -6,10 +6,10 @@
                 <h1 class="text-2xl xl:text-3xl font-extrabold">
                     Sign up
                 </h1>
-                <div class="w-full flex-1 mt-8">
-                    <div class="mx-auto max-w-xs">
+                <div class="w-full flex-1 mt-8 grid-cols-2">
+                    <div class="grid grid-rows-2 grid-cols-2 gap-1">
                         <input
-                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 col-span-2"
                             type="text" placeholder="Nombre Completo" 
                         />
                         <input
@@ -26,10 +26,6 @@
                             <option value="F">Femenino</option>
                             <option value="NB">No Binario</option>
                         </select>
-                        <input
-                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                            type="date" placeholder="Fecha de Nacimiento" 
-                        />
                         <select id="countries" class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5">
                             <option selected>Tipo de Sangre</option>
                             <option value="A+">A+</option>
@@ -42,33 +38,38 @@
                             <option value="O-">O-</option>
                         </select>
                         <input
-                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 col-span-2"
                             type="email" placeholder="Email" 
                         />
                         <input
-                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 mb-5"
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 "
                             type="password" placeholder="Contraseña" 
                         />
-                        <label for="" class="font-medium ">Fotografia</label>
                         <input
-                            class="w-full px-1 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-1"
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                            type="date" placeholder="Fecha de Nacimiento" 
+                        />
+                  
+                        <label for="" class="font-medium col-span-2 ">Fotografia</label>
+                        <input
+                            class="w-full px-1 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-1 col-span-2"
                             type="file" placeholder="Fotografia"
                         />
-                        <a href="/signup">
+                        <a href="/" class="col-span-2" >
                             <button
-                                class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                                class="mt-5 tracking-wide font-semibold bg-red-700 text-gray-100 w-full py-4 rounded-lg hover:bg-red-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none col-span-2">
                                 <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
                                     stroke-linecap="round" stroke-linejoin="round">
                                     <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
                                     <circle cx="8.5" cy="7" r="4" />
                                     <path d="M20 8v6M23 11h-6" />
                                 </svg>
-                                <span class="ml-3">
+                                <span class="ml-3 ">
                                     Sign Up
                                 </span>
                             </button>
                         </a>
-                        <p class="mt-6 text-xs text-gray-600 text-center">
+                        <p class="mt-6 text-xs text-gray-600 text-center col-span-2">
                             I agree to abide by templatana's
                             <a href="#" class="border-b border-gray-500 border-dotted">
                                 Terms of Service
@@ -82,9 +83,10 @@
                 </div>
             </div>
         </div>
-        <div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
-            <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-                style="background-image: url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg');">
+        <div class="flex-1 bg-gray-500 text-center hidden lg:flex ">
+            <div class="m-12 xl:m-16 w-10/12 bg-contain bg-center bg-no-repeat "
+                style="background-image: url('https://private-user-images.githubusercontent.com/163441777/323096598-75e27319-c2da-456b-8fc0-9bf3d29f6567.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTcwODA1OTQsIm5iZiI6MTcxNzA4MDI5NCwicGF0aCI6Ii8xNjM0NDE3NzcvMzIzMDk2NTk4LTc1ZTI3MzE5LWMyZGEtNDU2Yi04ZmMwLTliZjNkMjlmNjU2Ny5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNTMwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDUzMFQxNDQ0NTRaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT00YTM1ZGY3ODcyM2E0ZGNhMzk0N2M5YjU2OWM0MDQ0ZDlkNDdiODA5MjM1Yzk2MjU5NWI4NjU0MWEzNDMxM2E3JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.TqPJgXdHZUZQa11EYFEzoGOgJuUoVcCx4xrrP1CDGjg');"
+                >
             </div>
         </div>
     </div>

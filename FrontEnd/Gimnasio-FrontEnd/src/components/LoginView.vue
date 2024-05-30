a0ca0fb1f15a0c44b969f512d0debcfba2591024
adalid2608 <adalid.islas26@gmail.com>
2024-05-30 08:56:53 -0600

    Ajustes en los campos de inicio se Sesión

--- FrontEnd/Gimnasio-FrontEnd/src/components/LoginView.vue
+++ FrontEnd/Gimnasio-FrontEnd/src/components/LoginView.vue
@@ -2,10 +2,6 @@
 <div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
     <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
         <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-            <div>
-                <img src="https://storage.googleapis.com/devitary-image-host.appspot.com/15846435184459982716-LogoMakr_7POjrN.png"
-                    class="w-32 mx-auto" />
-            </div>
             <div class="mt-12 flex flex-col items-center">
                 <h1 class="text-2xl xl:text-3xl font-extrabold">
                     Sign up
@@ -13,7 +9,7 @@
                 <div class="w-full flex-1 mt-8">
                     <div class="flex flex-col items-center">
                         <button
-                            class="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
+                            class="w-full font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
                             <div class="bg-white p-2 rounded-full">
                                 <svg class="w-4" viewBox="0 0 533.5 544.3">
                                     <path
@@ -36,7 +32,7 @@
                         </button>
 
                         <button
-                            class="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
+                            class="w-full  font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
                             <div class="bg-white p-1 rounded-full">
                                 <svg class="w-6" viewBox="0 0 32 32">
                                     <path fill-rule="evenodd"
@@ -56,7 +52,7 @@
                         </div>
                     </div>
 
-                    <div class="mx-auto max-w-xs">
+                    <div class="w-full">
                         <input
                             class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
                             type="email" placeholder="Email" />
@@ -65,7 +61,7 @@
                             type="password" placeholder="Password" />
                         <a href="/signup">
                             <button
-                                class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                                class="mt-5 tracking-wide font-semibold bg-red-700 text-gray-100 w-full py-4 rounded-lg hover:bg-red-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
                                 <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
                                     stroke-linecap="round" stroke-linejoin="round">
                                     <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
@@ -91,9 +87,10 @@
                 </div>
             </div>
         </div>
-        <div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
-            <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-                style="background-image: url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg');">
+        <div class="flex-1 bg-gray-500 text-center hidden lg:flex ">
+            <div class="m-12 xl:m-16 w-10/12 bg-contain bg-center bg-no-repeat "
+                style="background-image: url('https://private-user-images.githubusercontent.com/163441777/323096598-75e27319-c2da-456b-8fc0-9bf3d29f6567.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTcwODA1OTQsIm5iZiI6MTcxNzA4MDI5NCwicGF0aCI6Ii8xNjM0NDE3NzcvMzIzMDk2NTk4LTc1ZTI3MzE5LWMyZGEtNDU2Yi04ZmMwLTliZjNkMjlmNjU2Ny5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNTMwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDUzMFQxNDQ0NTRaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT00YTM1ZGY3ODcyM2E0ZGNhMzk0N2M5YjU2OWM0MDQ0ZDlkNDdiODA5MjM1Yzk2MjU5NWI4NjU0MWEzNDMxM2E3JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.TqPJgXdHZUZQa11EYFEzoGOgJuUoVcCx4xrrP1CDGjg');"
+                >
             </div>
         </div>
     </div>

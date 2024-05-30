bc83fdea6c0a0f0a10b33ceae77589526c394c10
adalid2608 <adalid.islas26@gmail.com>
2024-05-30 10:55:58 -0600

    Creacion de la ruta para la pantalla de Menú

--- FrontEnd/Gimnasio-FrontEnd/src/router/index.js
+++ FrontEnd/Gimnasio-FrontEnd/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/components/LoginView.vue'
 import RegisterView from '@/components/RegisterView.vue'
+import Menu from '@/components/Menu.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +15,12 @@ const router = createRouter({
       path: '/signup',
       name: 'signup',
       component: RegisterView
-    }
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Menu
+    },
   ]
 })
 

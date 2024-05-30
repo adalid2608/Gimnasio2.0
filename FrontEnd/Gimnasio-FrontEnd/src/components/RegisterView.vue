b1a1ad02956a1ab977c83fd093c38a31e09c57f6
adalid2608 <adalid.islas26@gmail.com>
2024-05-30 10:55:27 -0600

    Actualizacion de la ruta para redireccion al Menú

--- FrontEnd/Gimnasio-FrontEnd/src/components/RegisterView.vue
+++ FrontEnd/Gimnasio-FrontEnd/src/components/RegisterView.vue
@@ -55,17 +55,21 @@
                             class="w-full px-1 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-1 col-span-2"
                             type="file" placeholder="Fotografia"
                         />
-                        <a href="/" class="col-span-2" >
+                        <a href="/menu">
+                            <button
+                                class="mt-5 tracking-wide font-semibold bg-green-700 text-gray-100 w-full py-4 rounded-lg hover:bg-green-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none col-span-2">
+                                
+                                <span>
+                                    Sign Up
+                                </span>
+                            </button>
+                        </a>
+                        <a href="/"  >
                             <button
                                 class="mt-5 tracking-wide font-semibold bg-red-700 text-gray-100 w-full py-4 rounded-lg hover:bg-red-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none col-span-2">
-                                <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round">
-                                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                                    <circle cx="8.5" cy="7" r="4" />
-                                    <path d="M20 8v6M23 11h-6" />
-                                </svg>
-                                <span class="ml-3 ">
-                                    Sign Up
+                                
+                                <span>
+                                    Cancel
                                 </span>
                             </button>
                         </a>

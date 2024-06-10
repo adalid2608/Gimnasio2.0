a65bffafccfba2d4748480d17ac41602a0ca1c3d
adalid2608 <adalid.islas26@gmail.com>
2024-06-10 16:37:57 -0600

    Rutas para cada uno de los componentes

--- FrontEnd/Gimnasio-FrontEnd/src/router/index.js
+++ FrontEnd/Gimnasio-FrontEnd/src/router/index.js
@@ -2,6 +2,37 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/components/LoginView.vue'
 import RegisterView from '@/components/RegisterView.vue'
 import Menu from '@/components/Menu.vue'
+import DashboardView from '@/components/views/DashboardView.vue'
+import SucursalView from '@/components/views/SucursalView.vue'
+import DietasView from '@/components/views/DietasView.vue'
+import PreguntaNutricionalView from '@/components/views/PreguntaNutricionalView.vue'
+import ValoracionNutricionalView from '@/components/views/ValoracionNutricionalView.vue'
+import EjerciciosView from '@/components/views/EjerciciosView.vue'
+import RutinasEjerciciosView from '@/components/views/RutinasEjerciciosView.vue'
+import RutinasView from '@/components/views/RutinasView.vue'
+import ProgramaSaludableView from '@/components/views/ProgramaSaludableView.vue'
+import DetalleProgramaView from '@/components/views/DetalleProgramaView.vue'
+import EquipamientoView from '@/components/views/EquipamientoView.vue'
+import PrestamosView from '@/components/views/PrestamosView.vue'
+import MembresiasView from '@/components/views/MembresiasView.vue'
+import MiembrosView from '@/components/views/MiembrosView.vue'
+import PersonasView from '@/components/views/PersonasView.vue'
+import AreasView from '@/components/views/AreasView.vue'
+import PuestosView from '@/components/views/PuestosView.vue'
+import EmpleadosView from '@/components/views/EmpleadosView.vue'
+import InstructoresView from '@/components/views/InstructoresView.vue'
+import HorariosView from '@/components/views/HorariosView.vue'
+import InstalacionesView from '@/components/views/InstalacionesView.vue'
+import MantenimientoView from '@/components/views/MantenimientoView.vue'
+import ProductosView from '@/components/views/ProductosView.vue'
+import DetallePromocionesView from '@/components/views/DetallePromocionesView.vue'
+import PedidoView from '@/components/views/PedidoView.vue'
+import DetallePedidosView from '@/components/views/DetallePedidosView.vue'
+import PagosView from '@/components/views/PagosView.vue'
+import PromocionesView from '@/components/views/PromocionesView.vue'
+import DetalleProductosView from '@/components/views/DetalleProductosView.vue'
+
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,75 +53,92 @@ const router = createRouter({
       component: Menu,
         children:[
           {
-            path:'/dashboard', name:'dashboard', component: RegisterView
+            path:'/dashboard', name:'dashboard', component: DashboardView
           },
           {
-            path:'/sucursal', name:'sucursal', component: RegisterView
+            path:'/sucursal', name:'sucursal', component: SucursalView
           },
           {
-            path:'/dietas', name:'dietas', component: RegisterView
+            path:'/dietas', name:'dietas', component: DietasView
           },
           {
-            path:'/preguntaNutricional', name:'preguntaNutricional', component: RegisterView
+            path:'/preguntaNutricional', name:'preguntaNutricional', component: PreguntaNutricionalView
           },
           {
-            path:'/valoracionNutricional', name:'valoracionNutricional', component: RegisterView
+            path:'/valoracionNutricional', name:'valoracionNutricional', component: ValoracionNutricionalView
           },
           {
-            path:'/ejercicios', name:'ejercicios', component: RegisterView
+            path:'/ejercicios', name:'ejercicios', component: EjerciciosView
           },
           {
-            path:'/rutinas', name:'rutinas', component: RegisterView
+            path:'/rutinas', name:'rutinas', component: RutinasView
           },
           {
-            path:'/programaSaludable', name:'programaSaludable', component: RegisterView
+            path:'/programaSaludable', name:'programaSaludable', component: ProgramaSaludableView
           },
           {
-            path:'/rutinasEjercicios', name:'rutinasEjercicios', component: RegisterView
+            path:'/rutinasEjercicios', name:'rutinasEjercicios', component: RutinasEjerciciosView
           },
           {
-            path:'/detalleProgramas', name:'detalleProgramas', component: RegisterView
+            path:'/detalleProgramas', name:'detalleProgramas', component: DetalleProgramaView
           },
           {
-            path:'/equipamiento', name:'equipamiento', component: RegisterView
+            path:'/equipamiento', name:'equipamiento', component: EquipamientoView
           },
           {
-            path:'/prestamos', name:'prestamos', component: RegisterView
+            path:'/prestamos', name:'prestamos', component: PrestamosView
           },
           {
-            path:'/membresias', name:'membresias', component: RegisterView
+            path:'/membresias', name:'membresias', component: MembresiasView
           },
           {
-            path:'/miembros', name:'miembros', component: RegisterView
+            path:'/miembros', name:'miembros', component: MiembrosView
           },
           {
-            path:'/personas', name:'personas', component: RegisterView
+            path:'/personas', name:'personas', component: PersonasView
           },
           {
-            path:'/areas', name:'areas', component: RegisterView
+            path:'/areas', name:'areas', component: AreasView
           },
           {
-            path:'/puestos', name:'puestos', component: RegisterView
+            path:'/puestos', name:'puestos', component: PuestosView
           },
           {
-            path:'/empleados', name:'empleados', component: RegisterView
+            path:'/empleados', name:'empleados', component: EmpleadosView
           },
           {
-            path:'/instructores', name:'intructores', component: RegisterView
+            path:'/instructores', name:'intructores', component: InstructoresView
           },
           {
-            path:'/horarios', name:'horarios', component: RegisterView
+            path:'/horarios', name:'horarios', component: HorariosView
           },
           {
-            path:'/sucursal', name:'sucursal', component: RegisterView
+            path:'/instalaciones', name:'instalaciones', component: InstalacionesView
           },
           {
-            path:'/instalaciones', name:'instalaciones', component: RegisterView
+            path:'/mantenimiento', name:'mantenimiento', component: MantenimientoView
           },
           {
-            path:'/mantenimiento', name:'mantenimiento', component: RegisterView
+            path:'/productos', name:'productos', component: ProductosView
           },
-          
+          {
+            path:'/detalleProductos', name:'detalleProductos', component: DetalleProductosView
+          },
+          {
+            path:'/promociones', name:'promociones', component: PromocionesView
+          },
+          {
+            path:'/detallePromociones', name:'detallePromociones', component: DetallePromocionesView
+          },
+          {
+            path:'/pedidos', name:'pedidos', component: PedidoView
+          },
+          {
+            path:'/detallePedidos', name:'detallePedidos', component: DetallePedidosView
+          },
+          {
+            path:'/pagos', name:'pagos', component: PagosView
+          },
         ]
     },
   ]

658c750009e5459e7b31172832b22eb7c7ffdc7f
adalid2608 <adalid.islas26@gmail.com>
2024-06-11 10:54:25 -0600

    Cambios en la tabla

--- FrontEnd/Gimnasio-FrontEnd/src/components/TablePersonas.vue
+++ FrontEnd/Gimnasio-FrontEnd/src/components/TablePersonas.vue
@@ -1,94 +1,99 @@
 <template>
-    <table class="min-w-full divide-y divide-gray-200 mt-8">
+    <table class=" w-full divide-y divide-gray-200 mt-8">
         <thead>
             <tr>
-                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fotografia</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Primer Apellido</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Segundo Apellido</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Genero</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tipo de Sangre</th>
                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
-                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Role</th>
-                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
-                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fecha de Nacimiento</th>
             </tr>
         </thead>
         <tbody class="bg-white divide-y divide-gray-200">
             <tr>
-                <td class="px-6 py-4 whitespace-nowrap">Jane Doe</td>
-                <td class="px-6 py-4 whitespace-nowrap">jane@example.com</td>
-                <td class="px-6 py-4 whitespace-nowrap">Admin</td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                    <button class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Edit</button>
-                    <button class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">Delete</button>
-                </td>
-            </tr>
-            <tr>
-                <td class="px-6 py-4 whitespace-nowrap">John Doe</td>
-                <td class="px-6 py-4 whitespace-nowrap">john@example.com</td>
-                <td class="px-6 py-4 whitespace-nowrap">User</td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">Inactive</span>
-                </td>
+                <td class="px-6 py-4 whitespace-nowrap"></td>
+                <td class="px-6 py-4 whitespace-nowrap">Adalid</td>
+                <td class="px-6 py-4 whitespace-nowrap">Islas</td>
+                <td class="px-6 py-4 whitespace-nowrap">Quintero</td>
+                <td class="px-6 py-4 whitespace-nowrap">Masculino</td>
+                <td class="px-6 py-4 whitespace-nowrap">A+</td>
+                <td class="px-6 py-4 whitespace-nowrap">adalid@gmail.com</td>
+                <td class="px-6 py-4 whitespace-nowrap">26/08/02</td>
                 <td class="px-6 py-4 whitespace-nowrap">
                     <button class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Edit</button>
                     <button class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">Delete</button>
                 </td>
             </tr>
             <tr>
-                <td class="px-6 py-4 whitespace-nowrap">John Doe</td>
-                <td class="px-6 py-4 whitespace-nowrap">john@example.com</td>
-                <td class="px-6 py-4 whitespace-nowrap">User</td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">Inactive</span>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                    <button class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Edit</button>
-                    <button class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">Delete</button>
-                </td>
-            </tr>
-            <tr>
-                <td class="px-6 py-4 whitespace-nowrap">John Doe</td>
-                <td class="px-6 py-4 whitespace-nowrap">john@example.com</td>
-                <td class="px-6 py-4 whitespace-nowrap">User</td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">Inactive</span>
-                </td>
+                <td class="px-6 py-4 whitespace-nowrap"></td>
+                <td class="px-6 py-4 whitespace-nowrap">Adalid</td>
+                <td class="px-6 py-4 whitespace-nowrap">Islas</td>
+                <td class="px-6 py-4 whitespace-nowrap">Quintero</td>
+                <td class="px-6 py-4 whitespace-nowrap">Masculino</td>
+                <td class="px-6 py-4 whitespace-nowrap">A+</td>
+                <td class="px-6 py-4 whitespace-nowrap">adalid@gmail.com</td>
+                <td class="px-6 py-4 whitespace-nowrap">26/08/02</td>
                 <td class="px-6 py-4 whitespace-nowrap">
                     <button class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Edit</button>
                     <button class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">Delete</button>
                 </td>
             </tr>
             <tr>
-                <td class="px-6 py-4 whitespace-nowrap">John Doe</td>
-                <td class="px-6 py-4 whitespace-nowrap">john@example.com</td>
-                <td class="px-6 py-4 whitespace-nowrap">User</td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">Inactive</span>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                    <button class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Edit</button>
-                    <button class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">Delete</button>
-                </td>
-            </tr>
-            <tr>
-                <td class="px-6 py-4 whitespace-nowrap">John Doe</td>
-                <td class="px-6 py-4 whitespace-nowrap">john@example.com</td>
-                <td class="px-6 py-4 whitespace-nowrap">User</td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">Inactive</span>
-                </td>
+                <td class="px-6 py-4 whitespace-nowrap"></td>
+                <td class="px-6 py-4 whitespace-nowrap">Adalid</td>
+                <td class="px-6 py-4 whitespace-nowrap">Islas</td>
+                <td class="px-6 py-4 whitespace-nowrap">Quintero</td>
+                <td class="px-6 py-4 whitespace-nowrap">Masculino</td>
+                <td class="px-6 py-4 whitespace-nowrap">A+</td>
+                <td class="px-6 py-4 whitespace-nowrap">adalid@gmail.com</td>
+                <td class="px-6 py-4 whitespace-nowrap">26/08/02</td>
                 <td class="px-6 py-4 whitespace-nowrap">
                     <button class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Edit</button>
                     <button class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">Delete</button>
                 </td>
             </tr>
             <tr>
-                <td class="px-6 py-4 whitespace-nowrap">John Doe</td>
-                <td class="px-6 py-4 whitespace-nowrap">john@example.com</td>
-                <td class="px-6 py-4 whitespace-nowrap">User</td>
                 <td class="px-6 py-4 whitespace-nowrap">
-                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">Inactive</span>
+                    <img src="https://flaticon.es/icono-gratis/foto_4299685?term=im%C3%A1genes+de+usuario&page=1&position=3&origin=tag&related_id=4299685" alt="imagen de usuario">
                 </td>
+                <td class="px-6 py-4 whitespace-nowrap">Adalid</td>
+                <td class="px-6 py-4 whitespace-nowrap">Islas</td>
+                <td class="px-6 py-4 whitespace-nowrap">Quintero</td>
+                <td class="px-6 py-4 whitespace-nowrap">Masculino</td>
+                <td class="px-6 py-4 whitespace-nowrap">A+</td>
+                <td class="px-6 py-4 whitespace-nowrap">adalid@gmail.com</td>
+                <td class="px-6 py-4 whitespace-nowrap">26/08/02</td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <button class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Edit</button>
+                    <button class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">Delete</button>
+                </td>
+            </tr>
+            <tr>
+                <td class="px-6 py-4 whitespace-nowrap"></td>
+                <td class="px-6 py-4 whitespace-nowrap">Adalid</td>
+                <td class="px-6 py-4 whitespace-nowrap">Islas</td>
+                <td class="px-6 py-4 whitespace-nowrap">Quintero</td>
+                <td class="px-6 py-4 whitespace-nowrap">Masculino</td>
+                <td class="px-6 py-4 whitespace-nowrap">A+</td>
+                <td class="px-6 py-4 whitespace-nowrap">adalid@gmail.com</td>
+                <td class="px-6 py-4 whitespace-nowrap">26/08/02</td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <button class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Edit</button>
+                    <button class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">Delete</button>
+                </td>
+            </tr>
+            <tr>
+                <td class="px-6 py-4 whitespace-nowrap"></td>
+                <td class="px-6 py-4 whitespace-nowrap">Adalid</td>
+                <td class="px-6 py-4 whitespace-nowrap">Islas</td>
+                <td class="px-6 py-4 whitespace-nowrap">Quintero</td>
+                <td class="px-6 py-4 whitespace-nowrap">Masculino</td>
+                <td class="px-6 py-4 whitespace-nowrap">A+</td>
+                <td class="px-6 py-4 whitespace-nowrap">adalid@gmail.com</td>
+                <td class="px-6 py-4 whitespace-nowrap">26/08/02</td>
                 <td class="px-6 py-4 whitespace-nowrap">
                     <button class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Edit</button>
                     <button class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">Delete</button>
--- FrontEnd/Gimnasio-FrontEnd/src/components/views/PersonasView.vue
+++ FrontEnd/Gimnasio-FrontEnd/src/components/views/PersonasView.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="flex">
-        <div class="w-1/2 text-gray-900 flex justify-center">
+    <div class="flex flex-col">
+        <div class="w-[30] text-gray-900 flex justify-center">
             <div class="m-0 sm:m-10 bg-white sm:rounded-lg flex justify-center flex-1">
                 <div class="w-full md:w-auto xl:w-5/12 p-6 sm:p-12">
                     <Persona></Persona>
             </div>
         </div>
     </div>
-        <div class="w-3/5 mt-32">
+        <div class="mt-12 w-full">
             <TablePersonas></TablePersonas>
         </div>
     </div>

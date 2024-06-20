217d5fce43aea44d5fdb8b2d28ba09889caf560d
adalid2608 <adalid.islas26@gmail.com>
2024-06-20 11:06:33 -0600

    Creación de nuevas rutas y modelo de usuarios

--- BackEnd/routes/user.py
+++ BackEnd/routes/user.py
@@ -1,6 +1,21 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from datetime import datetime
 
 user = APIRouter()
-@user.get("/user")
-def helloWorld():
+usersInfo = []
+
+class usersModel(BaseModel):
+    id: str
+    user: str
+    password: str
+    crated_at: datetime = datetime.now
+    status: bool = False
+
+@user.get("/")
+def bienvenida():
     return "Hola 9b"
+
+@user.get("/user")
+def usuarios():
+    return "Lista de Usuarios"

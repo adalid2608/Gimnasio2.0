8f4fa1a62e8664d2bc52b0598230101ed90464c9
adalid2608 <adalid.islas26@gmail.com>
2024-06-24 16:07:01 -0600

    Cambios en el archivo de user.py para nuevas rutas

--- BackEnd/routes/user.py
+++ BackEnd/routes/user.py
@@ -3,13 +3,7 @@ from pydantic import BaseModel
 from datetime import datetime
 
 user = APIRouter()
-users = [
-    # {"id":1,"user":"mgarbett0","password":"mF2*Wc~f","created_at":"8/25/2023","status":False},
-    # {"id":2,"user":"sscarre1","password":"yK2*(I?RJ)Pw.kPY","created_at":"7/22/2023","status":False},
-    # {"id":3,"user":"gmilliken2","password":"zK7>gX@U<q+","created_at":"5/6/2024","status":True},
-    # {"id":4,"user":"ihawkes3","password":"eH6_ISougz<xTG","created_at":"5/3/2024","status":True},
-    # {"id":5,"user":"hschechter4","password":"gZ4.#~pXN(fmm3X","created_at":"5/20/2024","status":False}
-]
+users = []
 
 class UsersModel(BaseModel):
     id: str

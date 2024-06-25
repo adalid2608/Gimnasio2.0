20a03a55b07168d6bd1f93479e460eb202a68846
adalid2608 <adalid.islas26@gmail.com>
2024-06-25 10:31:13 -0600

    Creación del modelo de personas y rutas para las 4 operaciones CRUD

--- BackEnd/routes/person.py
+++ BackEnd/routes/person.py
@@ -12,11 +12,33 @@ class PersonsModel(BaseModel):
     crated_at: datetime = datetime.now()
     status: bool = False
 
-@person.get("/persons")
+@person.get("/persons",tags=["Personas"])
 def get_persons():
     return persons
 
-@person.post("/persons")
+@person.post("/persons/{person_id}",tags=["Personas"])
+def get_persons(person_id:str):
+    for person in persons:
+        if person.id == person_id:
+            return person
+    return {"message": f"NO existe el registro"}
+
+@person.post("/persons/",tags=["Personas"])
 def save_persons(personInsert:PersonsModel):
     persons.append(personInsert)
     return "Datos Almacenados correctamente"
+
+@person.put("/persons/{person_id}",tags=["Personas"])
+def updateUser(update_person:PersonsModel, person_id:str):
+    for index, person in enumerate(persons):
+        if person.id == person_id:
+            update_person.crated_at = person.crated_at
+            persons[index] = update_person
+            return {"message": f"Se ah modificado una persona con el Id: {person_id}"}
+
+@person.delete("/persons/{person_id}",tags=["Personas"])
+def delete_user(person_id:str):
+    for index, person in enumerate(persons):
+        if person.id == person_id:
+            persons.pop(index)
+            return {"message": f"Se ah eliminado una persona con el Id: {person_id}"}
--- BackEnd/routes/user.py
+++ BackEnd/routes/user.py
@@ -12,15 +12,37 @@ class UsersModel(BaseModel):
     crated_at: datetime = datetime.now()
     status: bool = False
 
-@user.get("/")
+@user.get("/", tags=["Inicio"])
 def welcome():
     return "Hola 9b"
 
-@user.get("/users")
+@user.get("/users",tags=["Usuarios"])
 def get_users():
     return users
 
-@user.post("/users")
+@user.post("/users/{user_id}",tags=["Usuarios"])
+def get_persons(user_id:str):
+    for user in users:
+        if user.id == user_id:
+            return user
+    return {"message": f"NO existe el registro"}
+
+@user.post("/users/",tags=["Usuarios"])
 def save_users(userInsert:UsersModel):
     users.append(userInsert)
     return "Datos Almacenados correctamente"
+
+@user.put("/users/{user_id}",tags=["Usuarios"])
+def updateUser(update_user:UsersModel, user_id:str):
+    for index, user in enumerate(users):
+        if user.id == user_id:
+            update_user.crated_at = user.crated_at
+            users[index] = update_user
+            return {"message": f"Se ah modificado un usuario con el Id: {user_id}"}
+        
+@user.delete("/users/{user_id}",tags=["Usuarios"])
+def delete_user(user_id:str):
+    for index, user in enumerate(users):
+        if user.id == user_id:
+            users.pop(index)
+            return {"message": f"Se ah eliminado un usuario con el Id: {user_id}"}
